b3ae52b0e0dc6face8e90a49ccb7b9a37cf31103
Agut14 <carlosagut14@hotmail.com>
2022-09-22 19:21:33 +0200

    feat new routes, entity, persist, flush, etc

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,35 +14,84 @@ use Symfony\Component\Routing\Annotation\Route;
 class LibraryController extends AbstractController 
 {
 
-    #[Route('library/list', name: 'library_list')]
-    public function list(Request $request, LoggerInterface $logger): JsonResponse
+    #[Route('list', name: 'library_list')]
+    public function list(Request $request, LoggerInterface $logger, BookRepository $bookRepository): JsonResponse
     {
 
-        $title = $request->query->get('title', 'Up');
         $logger->info('List action called up');
+        $books = $bookRepository->findAll();
+        $booksAsArray = [];
+        foreach ($books as $book) {
+            $booksAsArray[] = [
+                'id'=> $book->getId(),
+                'title' => $book->getTitle(),
+                'image'=> $book->getImage(),
+            ];
+        }
         $response = new JsonResponse();
+        $response->setData([
+            'success' => true,
+            'data' => $booksAsArray
+        ]);
+        return $response;
+    }
+
+
+    #[Route('book', name: 'create_book')]
+    public function createBook(Request $request, EntityManagerInterface $em): JsonResponse{
+        $book = new Book();
+        $response = new JsonResponse();
+
+        $title = $request->get('title', null);
+        if(empty($title)){
+            $response->setData([
+               'success' => false,
+               'error' => 'Title is required',
+               'data' => null
+            ]);
+        }
+        
+        $book->setTitle($title);
+        $em->persist($book);
+        $em->flush();
+
         $response->setData([
             'success' => true,
             'data' => [
                 [
-                    'id' => 1,
-                    'title' => 'El Silmarillion',
-                    'autor' => 'Tolkien'
+                    'id' => $book->getId(),
+                    'title' => $title,
                 ],
-                [
-                    'id' => 2,
-                    'title' => 'Nacidos de la bruma',
-                    'autor' => 'Brandon Sanderson'
-                ],
-                [
-                    'id' => 3,
-                    'title' => $title
-                ]
             ]
         ]);
         return $response;
     }
 
+    #[Route('book/update/{id}', name: 'update_book')]
+    public function editBook(Request $request, EntityManagerInterface $em, BookRepository $bookRepository): JsonResponse{
+        $response = new JsonResponse();
+
+        $title = $request->get('title');
+        $id = $request->attributes->get('id');
+        $book = $bookRepository->find($id);
+        
+        
+        
+        $book->setTitle($title);
+        $em->persist($book);
+        $em->flush();
+
+        $response->setData([
+            'success' => true,
+            'data' => [
+                [
+                    'id' => $id,
+                    'title' => $title,
+                ],
+            ]
+        ]);
+        return $response;
+    }
 };
 
 
